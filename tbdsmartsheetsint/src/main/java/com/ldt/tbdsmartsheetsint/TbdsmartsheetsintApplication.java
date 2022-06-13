ab71fd4b3739eb465a742363516e016dedc04969
Luke Trickey <ldtrickey@gmail.com>
2022-06-13 08:44:28 -0400

    Initial Commit
    Create Java/Spring/Maven project

---
+++ tbdsmartsheetsint/src/main/java/com/ldt/tbdsmartsheetsint/TbdsmartsheetsintApplication.java
@@ -0,0 +1,13 @@
+package com.ldt.tbdsmartsheetsint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbdsmartsheetsintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TbdsmartsheetsintApplication.class, args);
+	}
+
+}
---
+++ tbdsmartsheetsint/src/test/java/com/ldt/tbdsmartsheetsint/TbdsmartsheetsintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ldt.tbdsmartsheetsint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbdsmartsheetsintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
